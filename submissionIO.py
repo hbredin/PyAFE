d3dded1febf8a7a9d5be43b20ae608e63f3275ed
Herve Bredin <bredin@irit.fr>
2010-07-28 13:52:44 +0200

    submissionIO: support for empty/inexistent submissions

--- submissionIO.py
+++ submissionIO.py
@@ -9,34 +9,43 @@ class Submission(object):
     def __init__(self, xmlroot):
         super(Submission, self).__init__()
         
-        if hasattr(xmlroot, 'submissionId'):
-            self.submissionId = xmlroot.submissionId
-        if hasattr(xmlroot, 'participantId'):
-            self.participantId = xmlroot.participantId
+        if xmlroot == None:
+            self.submissionId = None
+            self.participantId = None
+            self.ad_list = []
+            self.zik_list = []
+        else:
+            if hasattr(xmlroot, 'submissionId'):
+                self.submissionId = xmlroot.submissionId
+            if hasattr(xmlroot, 'participantId'):
+                self.participantId = xmlroot.participantId
 
-        if hasattr(xmlroot, 'detectionList'):
-            if hasattr(xmlroot.detectionList, 'Advertisement'):
-                all_ad = xmlroot.detectionList[0].Advertisement
-            else:
-                all_ad = []
-            if hasattr(xmlroot.detectionList, 'MusicTrack'):
-                all_zik = xmlroot.detectionList[0].MusicTrack
-            else:
-                all_zik = []
+            if hasattr(xmlroot, 'detectionList'):
+                if hasattr(xmlroot.detectionList, 'Advertisement'):
+                    all_ad = xmlroot.detectionList[0].Advertisement
+                else:
+                    all_ad = []
+                if hasattr(xmlroot.detectionList, 'MusicTrack'):
+                    all_zik = xmlroot.detectionList[0].MusicTrack
+                else:
+                    all_zik = []
         
-        self.ad_list = []
-        for one_ad in all_ad:
-    		self.ad_list.append(yacastIO.YacastAd(one_ad))
-    	self.ad_list.sort(yacastIO.YacastEvent.compareDate)
+            self.ad_list = []
+            for one_ad in all_ad:
+                self.ad_list.append(yacastIO.YacastAd(one_ad))
+            self.ad_list.sort(yacastIO.YacastEvent.compareDate)
 
-        self.zik_list = []
-        for one_zik in all_zik:
-    		self.zik_list.append(yacastIO.YacastZik(one_zik))
-    	self.zik_list.sort(yacastIO.YacastZik.compareDate)
+            self.zik_list = []
+            for one_zik in all_zik:
+                self.zik_list.append(yacastIO.YacastZik(one_zik))
+            self.zik_list.sort(yacastIO.YacastZik.compareDate)
 
 
 def loadSubmission(path2xml):
-    obj = objectify.parse(path2xml)
-    root = obj.getroot()
-    return Submission(root)
+    if path2xml == None:
+        return Submission(None)
+    else:
+        obj = objectify.parse(path2xml)
+        root = obj.getroot()
+        return Submission(root)
 
