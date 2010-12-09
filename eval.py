15ef7ca12b2afacd2f96510e1cf23d924c46c9ed
Herve Bredin <bredin@limsi.fr>
2010-12-09 16:32:12 +0100

    Make PyAFE GPL

--- eval.py
+++ eval.py
@@ -1,4 +1,21 @@
-# Code by Herve BREDIN (bredin@irit.fr) and Mathieu RAMONA (Mathieu.Ramona@ircam.fr)
+# Copyright 2010 Herve BREDIN (bredin@limsi.fr) and Mathieu RAMONA (Mathieu.Ramona@ircam.fr)
+# Contact: http://pyafe.niderb.fr/
+
+# This file is part of PyAFE.
+# 
+#     PyAFE is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     PyAFE is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with PyAFE.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import yacastIO
 import submissionIO
